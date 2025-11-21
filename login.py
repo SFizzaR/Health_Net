4d39ebeedf465a0c4d646d4c0f48710bf1896b02
Fizza <syedafizza.moin@gmail.com>
2025-11-21 23:48:27 +0500

    Add try/except

--- login.py
+++ login.py
@@ -1,29 +1,24 @@
 import sqlite3
 import hashlib
 import streamlit as st
+import utility.log as log
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def login_user(username, password):
-    conn = sqlite3.connect("hospital.db")
-    c = conn.cursor()
-    hashed = hash_password(password)
-    c.execute("SELECT role, user_id FROM users WHERE username=? AND password=?", (username, hashed))
-    data = c.fetchone()
-    conn.close()
-    if data:
-        role = data[0].lower()  # role
-        user_id = data[1] 
-        return role, user_id
-    else: 
-        st.warning("Login failed")
+    try:
+        conn = sqlite3.connect("hospital.db")
+        c = conn.cursor()
+        hashed = hash_password(password)
+        c.execute("SELECT role, user_id FROM users WHERE username=? AND password=?", (username, hashed))
+        data = c.fetchone()
+        conn.close()
+        if data:
+            role = data[0].lower()  # role
+            user_id = data[1] 
+            return role, user_id
+    except sqlite3.Error as e:
+        st.error("Error loging in.")
+        log.log_action(user_id, role, "error", f"ViewPermissions failed: {e}")
 
-
-def has_permission(role, permission):
-    conn = sqlite3.connect("hospital.db")
-    c = conn.cursor()
-    c.execute("SELECT 1 FROM roles_permissions WHERE role=? AND permission=?", (role, permission))
-    result = c.fetchone()
-    conn.close()
-    return bool(result)
